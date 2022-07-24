089cd7af5aac988cd6ba4bbc40db4bedb610496e
Abhishek Anand <anand2000ys@gmail.com>
2022-07-24 17:49:29 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      Hello World!
+
+
+      {/* Home (the one with the search on) */}
+
+
+
+
+      {/* SearchPage (The results page) */}
+    </div>
+  );
+}
+
+export default App;
