175b80cbc7292a961d6025a6b1c509876c3bd469
Abhishek Anand <anand2000ys@gmail.com>
2022-07-27 17:25:39 +0530

    React Router Installed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import React from 'react';
 import './App.css';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="app">
-      Hello World!
-
-
-      {/* Home (the one with the search on) */}
-
-
-
-
-      {/* SearchPage (The results page) */}
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>This is the search page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Home.css';
+
+function Home() {
+  return (
+    <div className='home'>
+      <h1>This is HOMEPAGE</h1>
+      <div className='home__header'>
+        <div className='home__headerleft'>
+          {/*Link*/}
+          {/*Link*/}
+        </div>
+        <div className='home__headerright'>
+          {/*Link*/}
+          {/*Link*/}
+          {/*Icon*/}
+          {/*Avatar*/}
+        </div>
+      </div>
+      <div className='home__body'>
+      </div>
+    </div>
+  )
+}
+
+export default Home
