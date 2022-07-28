359f0dc112e68f81bcb9c064496446fff4462cbb
Abhishek Anand <anand2000ys@gmail.com>
2022-07-28 16:43:18 +0530

    Header Added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Home.css';
+import { Link } from 'react-router-dom';
+import { Avatar } from '@mui/material';
+import AppsIcon from '@mui/icons-material/Apps';
 
 function Home() {
   return (
@@ -8,12 +11,18 @@ function Home() {
       <div className='home__header'>
         <div className='home__headerleft'>
           {/*Link*/}
+          <Link to='/about'>About</Link>
           {/*Link*/}
+          <Link to='/store'>Store</Link>
         </div>
         <div className='home__headerright'>
           {/*Link*/}
+          <Link to='/gmail'>Gmail</Link>
           {/*Link*/}
+          <Link to='/images'>Images</Link>
           {/*Icon*/}
+          <AppsIcon />
+          <Avatar />
           {/*Avatar*/}
         </div>
       </div>
