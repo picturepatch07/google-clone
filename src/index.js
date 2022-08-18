22f3a4796d3f320a8a8a663e8c3a7984930339d7
Abhishek Anand <anand2000ys@gmail.com>
2022-08-19 00:18:54 +0530

    Reducer Added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <StateProvider initialState={initialState} reducer={reducer} /> */}
+    <StateProvider initialState={initialState} reducer={reducer} >
+
     <App />
+    </StateProvider>
   </React.StrictMode>
 );
 
